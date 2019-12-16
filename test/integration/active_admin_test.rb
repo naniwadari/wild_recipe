b71485e6b510c72668e409a4c32dbf9de6d2e25d
dari <amanojaku52@hotmail.com>
2019-12-16 11:41:17 +0000

    add active_admin_test_name

--- test/integration/active_admin_test.rb
+++ test/integration/active_admin_test.rb
@@ -7,12 +7,14 @@ class ActiveAdminTest < ActionDispatch::IntegrationTest
     @non_admin = users(:recipeman)
   end
   
+  #管理者がactive_adminのマネジメント画面にアクセスできるか
   test "admin user should access management-screen" do
     log_in_as(@admin)
     get admin_root_path
     assert flash.empty?
   end
   
+  #未ログインユーザーが弾かれているか
   test "not logged in user should redirect to root_path" do
     get admin_root_path
     assert_redirected_to root_path
@@ -21,6 +23,7 @@ class ActiveAdminTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
   end
   
+  #管理者権限を持たない人が弾かれているか
   test "not admin user should redirect to root_path" do
     log_in_as(@non_admin)
     get admin_root_path
