24a61fc4bd4bd267ad0a211add9429cd0190e255
dari <amanojaku52@hotmail.com>
2019-12-25 14:36:02 +0000

    create basic ingredient-input-form

--- app/models/ingredient_collection.rb
+++ app/models/ingredient_collection.rb
@@ -16,11 +16,23 @@ class IngredientCollection
     @recipe = Recipe.find_by(id: recipe_id)
     #attributesに値が入っていればcollectionに材料データを入れ込んだ配列を代入する
     if attributes.present?
+      #nameとamountがどちらも空であればvalueをnilにする
       self.collection = attributes.map do |value|
-        @recipe.ingredient.build(
-          name: value[:name],
-          amount: value[:amount]
-        )
+        if value[:name].present? && value[:amount].present?
+          @recipe.ingredient.build(
+            name: value[:name],
+            amount: value[:amount]
+            )
+        end
+      end
+      #配列の要素がnilなら削除する
+      self.collection.delete_if{|value| value.nil?}
+      #配列の要素が材料数未満なら空の材料データを入れる
+      if self.collection.count <= INGREDIENT_NUM
+        rotation = INGREDIENT_NUM - self.collection.count
+        rotation.times do
+          self.collection.push( @recipe.ingredient.build )
+        end
       end
       #上で代入した材料データにナンバーを割り振る
       self.collection.each_with_index do |value, i|
@@ -36,8 +48,17 @@ class IngredientCollection
     is_success = true
     ActiveRecord::Base.transaction do
       collection.each do |result|
-        #バリデーションをかけたい場合はsave、そうでなければsave!(強制)
-        is_success = false unless result.save
+        recipe = Recipe.find_by(id: result.recipe_id)
+        ingre = recipe.ingredient.find_by(number: result.number)
+        #同ナンバーを持つ材料を書き換え
+        if ingre
+          ingre.update(name: result.name)
+          ingre.update(amount: result.amount)
+          is_success = false unless ingre.save
+        else
+          #バリデーションをかけたい場合はsave、そうでなければsave!(強制)
+          is_success = false unless result.save
+        end
       end
       #バリデーションエラーがあったときは例外を発生させてロールバックさせる
       raise ActiveRecord::RecordInvalid unless is_success
