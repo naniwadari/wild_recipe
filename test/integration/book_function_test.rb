c21da3d56f73e708b845d2e253dc6f5e32f9263e
dari <amanojaku52@hotmail.com>
2020-01-13 08:14:39 +0000

    fix book_function_test

--- test/integration/book_function_test.rb
+++ test/integration/book_function_test.rb
@@ -11,14 +11,16 @@ class BookFunctionTest < ActionDispatch::IntegrationTest
   test "successful book and unbook" do
     log_in_as(@user)
     get recipe_path(@recipe)
-    #ブックマークボタン
+    #ブックマークフォーム
     assert_select "form[action=?]", "/books"
     #データ送信
     assert_difference "Book.count", 1 do
       post books_path, params:{ recipe_id: @recipe.id }, xhr: true
     end
     get recipe_path(@recipe)
+    #ブックマーク解除フォーム
     assert_select "form[action=?]", "/books/#{(Book.last.id)}"
+    #解除データを送信
     assert_difference "Book.count", -1 do
       delete book_path(@user.books.find_by(recipe_id: @recipe.id)),
              params:{ recipe_id: @recipe.id}
