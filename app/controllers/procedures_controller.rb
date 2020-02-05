f347955fd176d1fda7b374261f946ff946587f41
dari <amanojaku52@hotmail.com>
2020-02-05 05:17:57 +0000

    調理の手順のフォームの表示方法を変更

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -1,25 +1,26 @@
 class ProceduresController < ApplicationController
   before_action :logged_in_author
-  before_action :correct_author_via
+  #before_action :correct_author_via
   
   def create
     @recipe = Recipe.find_by(id: params[:recipe_id])
     @procedure = @recipe.procedure.build(procedure_params)
     proce = @recipe.procedure.find_by(number: @procedure.number)
     #ナンバーがかぶっていた場合はアップデートする
-    if proce.present?
-      proce.update_attributes(content: @procedure.content)
-      respond_to do |format|
+    respond_to do |format|
+      if proce.present?
+        proce.update_attributes(content: @procedure.content)
+        format.html { redirect_to edit_recipe_path(@recipe) }
+        format.js
+        return
+      #かぶっていなければ普通にセーブ
+      elsif @procedure.save
+        format.html { redirect_to edit_recipe_path(@recipe) }
+        format.js
+      else
+        format.html { redirect_to edit_recipe_path(@recipe) }
         format.js
       end
-      return
-    #かぶっていなければ普通にセーブ
-    elsif @procedure.save
-      respond_to do |format|
-        format.js
-      end
-    else
-      render "recipes/edit"
     end
   end
   
@@ -28,12 +29,12 @@ class ProceduresController < ApplicationController
     @procedure = @recipe.procedure.find_by(number: params[:number])
     @next_procedure = @procedure.next_number
     respond_to do |format|
-      if @next_procedure.present?
-        Procedure.change_content(@procedure, @next_procedure)
+      if Procedure.change_content(@procedure, @next_procedure)
         format.html { redirect_to edit_recipe_path(@recipe)}
         format.js
       else
-        redirect_to edit_recipe_path(@recipe)
+        format.html { redirect_to edit_recipe_path(@recipe)}
+        format.js
       end
     end
   end
@@ -43,17 +44,25 @@ class ProceduresController < ApplicationController
     @procedure = @recipe.procedure.find_by(number: params[:number])
     @previous_procedure = @procedure.previous_number
     respond_to do |format|
-      if @previous_procedure.present?
-        Procedure.change_content(@procedure, @previous_procedure)
+      if Procedure.change_content(@procedure, @previous_procedure)
         format.html { redirect_to edit_recipe_path(@recipe)}
         format.js
       else
-        return false
+        format.html { redirect_to edit_recipe_path(@recipe)}
+        format.js
       end
     end
   end
   
   def destroy
+    @procedure = Procedure.find(params[:id])
+    @recipe = @procedure.recipe
+    @procedure.destroy_procedure
+    redirect_to edit_recipe_path(@recipe)
+    #respond_to do |format|
+    #  format.html { redirect_to edit_recipe_path(@recipe) }
+    #  format.js
+    #end
   end
   
   private 
--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -2,10 +2,14 @@ class Procedure < ApplicationRecord
   belongs_to :recipe
   
   #コンテンツを入れ替える
-  def self.change_content(procedure, change_procedure)
+  def self.change_content(procedure = nil, change_procedure = nil)
+    begin
       temp_content = procedure.content
       procedure.update_attributes(content: change_procedure.content)
       change_procedure.update_attributes(content: temp_content)
+    rescue => e
+      return false
+    end
   end
   
   #ナンバーが次に大きいレコードを取得
@@ -16,5 +20,18 @@ class Procedure < ApplicationRecord
   #ナンバーが次に小さいレコードを取得
   def previous_number
     Procedure.where("(number < ?) AND (recipe_id = ?)", self.number, self.recipe_id).order("number DESC").first
+  end
+  
+  #ナンバーが下のレコードのナンバーを-1して削除する
+  def destroy_procedure
+    self.lower_number.each do |procedure|
+      procedure.update_attributes(number: (procedure.number - 1))
+    end
+    self.destroy
+  end
+  
+  #自分が持つナンバーより下のナンバーを持つレコードを取得
+  def lower_number
+    Procedure.where("(number > ?) AND (recipe_id = ?)", self.number, self.recipe_id)
   end
 end
