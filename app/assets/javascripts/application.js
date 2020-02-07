9e23b8fc7b74978c621b8f9fdc7799d396f97ad8
松野　浩士 <koji@matsunohiroshinoMacBook-Pro.local>
2020-02-07 09:22:20 +0900

    プロフィールページにタブ追加中

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,4 +26,15 @@ $(document).on('turbolinks:load', function(){
     });
   });
   
+  $(function(){
+    $(".profile-tab").click(function(){
+      $(".is-active").removeClass("is-active");
+      $(this).addClass("is-active");
+      $(".is-show").removeClass("is-show");
+      //クリックしたタブからインデックス番号を取得
+      const index = $(this).index();
+      //クリックしたタブと同じインデックス番号をもつコンテンツを表示
+      $(".panel").eq(index).addClass("is-show");
+    });
+  });
 });
