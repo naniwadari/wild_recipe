03213d59c5c83bc13890957a69377b3d3ca99ed0
松野浩士 <amanojac52@gmail.com>
2020-02-28 21:49:07 +0900

    環境変数管理に変更

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -18,10 +18,11 @@ CarrierWave.configure do |config|
 			provider: 'AWS',
 			region: 'ap-northeast-1',
 			#credentialsで管理
-			aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-			aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
-			#:aws_access_key_id => ENV["AWS_ACCESS_KEY_ID"],
-			#:aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+			#aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+			#aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
+			#環境変数で管理
+			:aws_access_key_id => ENV["AWS_ACCESS_KEY_ID"],
+			:aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
 		}
 	else
 		#開発環境はlocalに保存
