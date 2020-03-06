7c1e483f30a64cda3ee00d19ec23badc59ae0fa1
松野浩士 <amanojac52@gmail.com>
2020-03-06 23:13:12 +0900

    impressionのlogged_in_userメソッドを追加

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -1,6 +1,6 @@
 class ImpressionsController < ApplicationController
   before_action :correct_author,only: :destroy
-  
+  before_action :logged_in_user
   def create
     @recipe = Recipe.find_by(id: params[:recipe_id])
     current_user.write_comment(@recipe, params[:comment])
@@ -21,6 +21,14 @@ class ImpressionsController < ApplicationController
     unless current_user == @comment.user
       redirect_to root_url
       flash[:danger] = "編集権限がありません"
+    end
+  end
+
+  def logged_in_user
+    @recipe = Recipe.find_by(id: params[:recipe_id])
+    unless logged_in?
+      flash[:danger] = "コメントを書き込むにはログインしてください"
+      redirect_to @recipe
     end
   end
 end
