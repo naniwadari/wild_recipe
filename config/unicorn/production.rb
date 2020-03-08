ce227be6957fd8ea39100232c805f5e11490893a
松野浩士 <amanojac52@gmail.com>
2020-03-08 20:35:18 +0900

    config/unicorn/production.rb記述

---
+++ config/unicorn/production.rb
@@ -0,0 +1,41 @@
+#ワーカーの数
+$worker = 2
+#何秒経過すればワーカーを削除するのか
+$timeout = 30
+#自分のアプリケーション名
+$app_dir = "/var/www/rails/wild_recipe/current"
+#リクエストを受け取るポート番号を指定
+$listen = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
+#PIDの管理ファイルディレクトリ
+$pid = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+#エラーログを吐き出すディレクトリ
+$std_log = File.expand_path 'log/unicorn.log', $app_dir
+
+#上記設定の定義
+worker_processes $worker
+woking_directory $app_dir
+stderr_path $std_log
+stdout_path $std_log
+timeout $timeout
+listen $listen
+pid $pid
+
+#ホットデプロイの有無
+preload_app true
+
+#fork前に行うことを定義
+before_fork do |server,worker|
+	defined?(ActiveRecord::Base) and ActiveRecord::connection.disconnect!
+	old_pid = "#{server.config[:pid]}.oldbin"
+	if old_pid != server.pid
+		begin
+			process.kill "QUIT", File.read(old_pid).to_i
+		rescue Errno::ENOENT, Errno::ESRCH
+		end
+	end
+end
+
+#fork後に行うことを定義
+after_fork do |server,worker|
+	defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
