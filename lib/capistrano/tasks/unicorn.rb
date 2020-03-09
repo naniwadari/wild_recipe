46696c44fc41e8406fc0ee005679df083511f87b
松野浩士 <amanojac52@gmail.com>
2020-03-09 17:56:37 +0900

    unicorn.rb変更

--- lib/capistrano/tasks/unicorn.rb
+++ lib/capistrano/tasks/unicorn.rb
@@ -3,6 +3,7 @@ namespace :unicorn do
 	task :environment do
 		set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
 		set :unicorn_config, "#{current_path}/config/unicorn/production.rb"
+	end
 end
 
 #unicornを開始
