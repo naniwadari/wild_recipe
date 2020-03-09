4edb77570a12375405e239ac7b890ad84bad14e0
松野浩士 <amanojac52@gmail.com>
2020-03-09 18:01:11 +0900

    unicorn.rb修正

--- lib/capistrano/tasks/unicorn.rb
+++ lib/capistrano/tasks/unicorn.rb
@@ -4,62 +4,63 @@ namespace :unicorn do
 		set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
 		set :unicorn_config, "#{current_path}/config/unicorn/production.rb"
 	end
-end
 
-#unicornを開始
-def start_unicorn
-	within current_path do
-		execute :bundle, :exec, :unicorn, "-c #{fetch(:unicorn_config)} -E #{fetch(:rails_env)} -D"
+	#unicornを開始
+	def start_unicorn
+		within current_path do
+			execute :bundle, :exec, :unicorn, "-c #{fetch(:unicorn_config)} -E #{fetch(:rails_env)} -D"
+		end
 	end
-end
 
-#unicornを停止
-def stop_unicorn
-	execute :kill, "-s QUIT $(< #{fetch(:unicorn_pid)})"
-end
-
-#unicornを再起動
-def reload_unicorn
-	execute :kill, "-s USR2 $(< #{fetch(:unicorn_pid)})"
-end
-
-#unicornを強制終了するメソッド
-def force_stop_unicorn
-	execute :kill, "$(< #{fetch(:unicorn_pid)})"
-end
-
-#unicornをスタートさせるtask
-desc "Start unicorn server"
-task start: :environment do
-	on roles(:app) do
-		start_unicorn
+	#unicornを停止
+	def stop_unicorn
+		execute :kill, "-s QUIT $(< #{fetch(:unicorn_pid)})"
 	end
-end
 
-#unicornを停止させるtask
-desc "Stop unicorn server gracefully"
-task stop: :environment do
-	on roles(:app) do
-		stop_unicorn
+	#unicornを再起動
+	def reload_unicorn
+		execute :kill, "-s USR2 $(< #{fetch(:unicorn_pid)})"
 	end
-end
 
-#既にunicornが起動している場合再起動を、まだの場合起動を行うtask
-desc "Restart unicorn server gracefully"
-task restart: :environment do
-	on roles(:app) do
-		if test("[ -f #{fetch(:unicorn_pid)} ]")
-			reload_unicorn
-		else
+	#unicornを強制終了するメソッド
+	def force_stop_unicorn
+		execute :kill, "$(< #{fetch(:unicorn_pid)})"
+	end
+
+	#unicornをスタートさせるtask
+	desc "Start unicorn server"
+	task start: :environment do
+		on roles(:app) do
 			start_unicorn
 		end
 	end
-end
 
-#unicornを強制終了させるtask
-desc "Stop unicorn server immediately"
-task force_stop: :environment do
-	on roles(:app) do
-		force_stop_unicorn
+	#unicornを停止させるtask
+	desc "Stop unicorn server gracefully"
+	task stop: :environment do
+		on roles(:app) do
+			stop_unicorn
+		end
 	end
+
+	#既にunicornが起動している場合再起動を、まだの場合起動を行うtask
+	desc "Restart unicorn server gracefully"
+	task restart: :environment do
+		on roles(:app) do
+			if test("[ -f #{fetch(:unicorn_pid)} ]")
+				reload_unicorn
+			else
+				start_unicorn
+			end
+		end
+	end
+
+	#unicornを強制終了させるtask
+	desc "Stop unicorn server immediately"
+	task force_stop: :environment do
+		on roles(:app) do
+			force_stop_unicorn
+		end
+	end
+
 end
