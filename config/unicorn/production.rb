83a76e10fc22d5d832ea3a75303e794ce9fd1dba
松野浩士 <amanojac52@gmail.com>
2020-03-09 22:44:37 +0900

    unicorn/production.rb 28l修正

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -25,7 +25,7 @@ preload_app true
 
 #fork前に行うことを定義
 before_fork do |server,worker|
-	defined?(ActiveRecord::Base) and ActiveRecord::connection.disconnect!
+	defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 	old_pid = "#{server.config[:pid]}.oldbin"
 	if old_pid != server.pid
 		begin
