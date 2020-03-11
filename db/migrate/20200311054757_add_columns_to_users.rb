62c4e8e40c39cf20f9f19fc25dc4d8d560bc8497
松野浩士 <amanojac52@gmail.com>
2020-03-11 15:39:30 +0900

    twitter用のカラムをマイグレート

---
+++ db/migrate/20200311054757_add_columns_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :uid, :string
+    add_column :users, :provider, :string
+    add_column :users, :nickname, :string
+    add_column :users, :location, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200308040252) do
+ActiveRecord::Schema.define(version: 20200311054757) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -96,6 +96,10 @@ ActiveRecord::Schema.define(version: 20200308040252) do
     t.string "remember_digest"
     t.text "profile_text"
     t.string "image"
+    t.string "uid"
+    t.string "provider"
+    t.string "nickname"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
